b2a706e992c6e3af015c7384d2fe96116f42a648
AlexeyTipunin <aleksey.tipunin@playgendary.com>
2023-08-16 21:43:50 +0300

    add scale

--- Assets/Project/Scripts/UI/PromoView/ProductIconView.cs
+++ Assets/Project/Scripts/UI/PromoView/ProductIconView.cs
@@ -4,18 +4,22 @@ using RedPanda.Project.Interfaces;
 using TMPro;
 using UnityEngine.EventSystems;
 using System;
+using System.Collections;
 
 namespace RedPanda.Project.Assets.Project.Scripts.UI.PromoView
 {
     public class ProductIconView : MonoBehaviour, IPointerClickHandler
     {
         public event Action<IPromoModel> OnBuyClick;
-        
+
         [SerializeField] private Image _productBack;
         [SerializeField] private Image _productIcon;
 
         [SerializeField] private TMP_Text _cost;
         [SerializeField] private TMP_Text _productName;
+
+        [SerializeField] private float _clickScale = 1.04f;
+        [SerializeField] private float _scaleDuaration = 0.2f;
 
         private IPromoModel _productModel;
 
@@ -32,7 +36,34 @@ namespace RedPanda.Project.Assets.Project.Scripts.UI.PromoView
 
         public void OnPointerClick(PointerEventData eventData)
         {
+            StartCoroutine(PlayScale());
             OnBuyClick?.Invoke(_productModel);
+        }
+
+        private IEnumerator PlayScale()
+        {
+            float targetScale = 1.04f;
+            float halfTime = _scaleDuaration / 2;
+            float elapsed = 0;
+
+            while (elapsed < halfTime)
+            {
+                elapsed += Time.deltaTime;
+                var scale = Mathf.Lerp(1, targetScale, elapsed / halfTime);
+                transform.localScale = new Vector3(scale, scale, 1);
+                yield return null;
+            }
+
+            elapsed = 0;
+            while (elapsed < halfTime)
+            {
+                elapsed += Time.deltaTime;
+                var scale = Mathf.Lerp(targetScale, 1, elapsed / halfTime);
+                transform.localScale = new Vector3(scale, scale, 1);
+                yield return null;
+            }
+
+            transform.localScale = new Vector3(1, 1, 1);
         }
     }
 }
